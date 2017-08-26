58ca15cee15aa868622f94deb4435d804ba560d5
Jakub Sanecki <jakubsanecki@MacBook-Pro-Jakub.local>
2017-08-26 15:09:50 +0200

    Initial Commit

---
+++ NannyScreen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NannyScreen
+//
+//  Created by Jakub Sanecki on 26.08.2017.
+//  Copyright © 2017 Jakub Sanecki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
